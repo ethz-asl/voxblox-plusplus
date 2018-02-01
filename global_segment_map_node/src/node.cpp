23166c019e6451944699e7234f3d2d446c0874e1
Fadri Furrer <ffurrer@gmail.com>
2018-02-01 13:18:50 +0100

    publish all segments in the end and online publish if segments are large enough

--- global_segment_map_node/src/node.cpp
+++ global_segment_map_node/src/node.cpp
@@ -1,14 +1,18 @@
 // Copyright 2017 Margarita Grinvald, ASL, ETH Zurich, Switzerland
 
 #include <glog/logging.h>
+#include <gflags/gflags.h>
 #include <ros/ros.h>
 
 #include "voxblox_gsm/controller.h"
 
 int main(int argc, char** argv) {
-  google::InitGoogleLogging("gsm_node");
-
   ros::init(argc, argv, "gsm_node");
+  google::InitGoogleLogging(argv[0]);
+  google::ParseCommandLineFlags(&argc, &argv, false);
+  google::InstallFailureSignalHandler();
+
+  LOG(INFO) << "Running GSM...";
 
   ros::NodeHandle node_handle;
   ros::NodeHandle node_handle_private("~");
@@ -42,6 +46,13 @@ int main(int argc, char** argv) {
   ros::ServiceServer extract_segments_srv;
   controller.advertiseExtractSegmentsService(&extract_segments_srv);
 
-  ros::spin();
+  constexpr double kNoUpdateTimeout = 20.0;
+  while (ros::ok() && !controller.noNewUpdatesReceived(kNoUpdateTimeout)) {
+    ros::spinOnce();
+  }
+  LOG(INFO) << "Shutting down";
+  controller.publishScene();
+  constexpr bool kPublishAllSegments = true;
+  controller.publishObjects(kPublishAllSegments);
   return 0;
 }
