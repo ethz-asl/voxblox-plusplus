739339c37d7cc054025c4ebc94320d867d07ca2b
SebasRatz <sratz@student.ethz.ch>
2018-05-07 11:09:34 +0200

    * sliding window now starts only after the first pointcloud input was received

--- global_segment_map_node/include/voxblox_gsm/sliding_window_controller.h
+++ global_segment_map_node/include/voxblox_gsm/sliding_window_controller.h
@@ -40,6 +40,7 @@ class SlidingWindowController : public Controller {
   Point current_window_position_point_;
   std::vector<Label> removed_segments_;
   float window_radius_ = 1.0f;
+  bool window_has_moved_first_time_= false;
 };
 }  // namespace voxblox_gsm
 }  // namespace voxblox
