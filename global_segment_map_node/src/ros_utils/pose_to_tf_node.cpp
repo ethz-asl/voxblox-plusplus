32ec4ee7d930bfe3a4e1f2ff526d76aff3bca2d3
SebasRatz <sratz@student.ethz.ch>
2018-06-13 18:35:53 +0200

    * adapt pose to tf publisher such that in corrects for differences in tango and rovioli calibration poses

--- global_segment_map_node/src/ros_utils/pose_to_tf_node.cpp
+++ global_segment_map_node/src/ros_utils/pose_to_tf_node.cpp
@@ -17,6 +17,10 @@ class PoseToTfNode {
   void PoseStampedToTransformStamped(const geometry_msgs::PoseStamped& pose,
                                      tf::StampedTransform* tf);
 
+  void getAndPublishT_I_D();
+  Eigen::Matrix4f getMatrix(std::vector<double> quaternions, std::vector<double> translation);
+  void publishTransform(const Eigen::Matrix4f matrix, std::string frame_id, std::string child_frame_id);
+
   ros::NodeHandle node_handle_;
   ros::Subscriber markers_sub;
   tf::TransformBroadcaster tf_broadcaster;
@@ -31,41 +35,18 @@ class PoseToTfNode {
 
 PoseToTfNode::PoseToTfNode(ros::NodeHandle& node_handle)
     : node_handle_(node_handle) {
-  std::vector<double> translation;
-  std::vector<double> rotation;
-  node_handle_.getParam("/rovioli_marker_to_tf/T_I_D/quaternion", rotation);
-  node_handle_.getParam("/rovioli_marker_to_tf/T_I_D/translation_m", translation);
 
   std::string topic = "/maplab_rovio/T_G_I";
   node_handle.param<std::string>("/pose_to_tf/pose_topic", topic, topic);
   markers_sub =
       node_handle_.subscribe(topic, 2000, &PoseToTfNode::newPoseCallback, this);
 
-  tf::Quaternion rot(rotation[0], rotation[1], rotation[2], rotation[3]);
-  tf::Vector3 trans(translation[0], translation[1], translation[2]);
-  geometry_msgs::TransformStamped tf_imu_depth;
-  tf_imu_depth.transform.rotation.x = rot.x();
-  tf_imu_depth.transform.rotation.y = rot.y();
-  tf_imu_depth.transform.rotation.z = rot.z();
-  tf_imu_depth.transform.rotation.w = rot.w();
-  tf_imu_depth.transform.translation.x = trans.x();
-  tf_imu_depth.transform.translation.y = trans.y();
-  tf_imu_depth.transform.translation.z = trans.z();
-  tf_imu_depth.header.frame_id = imu_estimated_frame_;
-  tf_imu_depth.child_frame_id = depth_estimated_frame_;
-  tf_static.sendTransform(tf_imu_depth);
+  getAndPublishT_I_D();
 
-  geometry_msgs::TransformStamped tf_world_map;
-  tf_world_map.transform.translation.x = 0;
-  tf_world_map.transform.translation.y = 0;
-  tf_world_map.transform.translation.z = 0;
-  tf_world_map.transform.rotation.x = 0;
-  tf_world_map.transform.rotation.y = 0;
-  tf_world_map.transform.rotation.z = 0;
-  tf_world_map.transform.rotation.w = 1;
-  tf_world_map.header.frame_id = map_frame_;
-  tf_world_map.child_frame_id = world_frame_;
-  tf_static.sendTransform(tf_world_map);
+  // T map to world
+  Eigen::Matrix4f T_World_Map;
+  T_World_Map.setIdentity();
+  publishTransform(T_World_Map, world_frame_, map_frame_);
 }
 
 void PoseToTfNode::newPoseCallback(const geometry_msgs::PoseStamped& pose_msg) {
@@ -76,8 +57,8 @@ void PoseToTfNode::newPoseCallback(const geometry_msgs::PoseStamped& pose_msg) {
 
 void PoseToTfNode::PoseStampedToTransformStamped(
     const geometry_msgs::PoseStamped& pose, tf::StampedTransform* tf) {
-  tf->child_frame_id_ = imu_estimated_frame_;
   tf->frame_id_ = pose.header.frame_id;
+  tf->child_frame_id_ = imu_estimated_frame_;
   tf->stamp_ = pose.header.stamp;
   tf->setRotation(
       tf::Quaternion(pose.pose.orientation.x, pose.pose.orientation.y,
@@ -87,6 +68,66 @@ void PoseToTfNode::PoseStampedToTransformStamped(
                             pose.pose.position.z));
 }
 
+Eigen::Matrix4f PoseToTfNode::getMatrix(std::vector<double> quaternions,
+                                        std::vector<double> translation) {
+  Eigen::Quaternionf quaternion(quaternions[3], quaternions[0], quaternions[1], quaternions[2]);
+  Eigen::Vector3f base;
+  Eigen::Matrix4f tf;
+  tf.setIdentity();
+  tf.block<3,3>(0,0) = quaternion.toRotationMatrix();
+  tf.block<3,1>(0,3) << translation[0] , translation[1], translation[2];
+  return tf;
+}
+
+
+void PoseToTfNode::getAndPublishT_I_D() {
+  std::vector<double> translation;
+  std::vector<double> rotation;
+
+  // T Imu tango to Depth
+  node_handle_.getParam("/rovioli_marker_to_tf/T_I_D/quaternion", rotation);
+  node_handle_.getParam("/rovioli_marker_to_tf/T_I_D/translation_m", translation);
+  Eigen::Matrix4f T_T_D = getMatrix(rotation, translation);
+
+  // T imu tango to fisheye
+  node_handle_.getParam("/rovioli_marker_to_tf/T_T_F/quaternion", rotation);
+  node_handle_.getParam("/rovioli_marker_to_tf/T_T_F/translation_m", translation);
+  Eigen::Matrix4f T_T_F = getMatrix(rotation, translation);
+
+  // T imu rovioli to fisheye
+  std::vector<double> matrix_as_vector;
+  node_handle_.getParam("/rovioli_marker_to_tf/T_R_F/matrix", matrix_as_vector);
+
+  Eigen::Matrix4f T_R_F;
+  for (size_t i=0; i<4; ++i) {
+    for (size_t j=0; j<4; ++j) {
+      T_R_F(i,j) = matrix_as_vector[4*i + j];
+    }
+  }
+
+  LOG(INFO) << "TID" << T_T_D << "TTF" << T_T_F << "TRF" << T_R_F;
+  Eigen::Matrix4f T_R_D;
+  T_R_D = T_R_F * T_T_F.inverse() * T_T_D;
+
+  publishTransform(T_R_D, imu_estimated_frame_, depth_estimated_frame_);
+}
+
+void PoseToTfNode::publishTransform(const Eigen::Matrix4f matrix, std::string frame_id, std::string child_frame_id) {
+  LOG(INFO) << "Publishing tf " << frame_id << " to " << child_frame_id << std::endl << matrix;
+  geometry_msgs::TransformStamped tf;
+  Eigen::Quaternionf quat(matrix.block<3,3>(0,0));
+  tf.transform.rotation.x = quat.x();
+  tf.transform.rotation.y = quat.y();
+  tf.transform.rotation.z = quat.z();
+  tf.transform.rotation.w = quat.w();
+  tf.transform.translation.x = matrix(0,3);
+  tf.transform.translation.y = matrix(1,3);
+  tf.transform.translation.z = matrix(2,3);
+  tf.header.frame_id = frame_id;
+  tf.child_frame_id = child_frame_id;
+  tf_static.sendTransform(tf);
+
+}
 }
 }
 
