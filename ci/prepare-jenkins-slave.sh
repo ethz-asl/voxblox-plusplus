bb2d7b5744f9dc82c54a4a7f6803658ecd5e0344
ntonci <tonci.novkovic@gmail.com>
2019-04-18 15:35:15 +0200

    Trigger build of approxmvbb in cmake instead of prepare script

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -1,11 +1,3 @@
 #!/bin/bash -e
 echo "Running the prepare script for voxblox_gsm.";
 catkin config --cmake-args -DCMAKE_CXX_STANDARD=14
-catkin config --merge-devel 
-
-sudo apt-get install bc
-CMAKE_VERSION=$(cmake --version | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p')
-MIN_CMAKE_VERSION=3.10
-if (($(echo "${CMAKE_VERSION} >= ${MIN_CMAKE_VERSION}" | bc -l) )); then
-  catkin build approxmvbb_catkin
-fi
