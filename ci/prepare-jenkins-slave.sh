cc10ce096c0bcbd38dd135119ebffe661f68e18d
ntonci <tonci.novkovic@gmail.com>
2019-04-18 12:53:21 +0200

    Fix build command

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -6,5 +6,5 @@ sudo apt-get install bc
 CMAKE_VERSION=$(cmake --version | sed -ne 's/[^0-9]*\(\([0-9]\.\)\{0,4\}[0-9][^.]\).*/\1/p')
 MIN_CMAKE_VERSION=3.10
 if (($(echo "${CMAKE_VERSION} >= ${MIN_CMAKE_VERSION}" | bc -l) )); then
-  catkin build approxmvbb_catkin --merge-devel
+  catkin build approxmvbb_catkin
 fi
