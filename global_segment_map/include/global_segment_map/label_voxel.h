d42a42fd511a5db72ea16aa578cff9370a8076f3
ntonci <tonci.novkovic@gmail.com>
2019-06-19 13:28:11 +0200

    Addressed comments and added missing headers

--- global_segment_map/include/global_segment_map/label_voxel.h
+++ global_segment_map/include/global_segment_map/label_voxel.h
@@ -5,7 +5,6 @@
 #include <string>
 
 #include <voxblox/core/voxel.h>
-#include <voxblox/utils/layer_utils.h>
 
 #include "global_segment_map/common.h"
 
@@ -26,23 +25,6 @@ inline std::string getVoxelType<LabelVoxel>() {
   return voxel_types::kLabel;
 }
 
-namespace utils {
-template <>
-inline bool isSameVoxel(const LabelVoxel& voxel_A, const LabelVoxel& voxel_B) {
-  bool is_the_same = true;
-
-  is_the_same &= voxel_A.label == voxel_B.label;
-  is_the_same &= voxel_A.label_confidence == voxel_B.label_confidence;
-
-  for (int i = 0; i < 40; ++i) {
-    is_the_same &= voxel_A.label_count[i].label == voxel_B.label_count[i].label;
-    is_the_same &= voxel_A.label_count[i].label_confidence ==
-                   voxel_B.label_count[i].label_confidence;
-  }
-
-  return is_the_same;
-}
-}  // namespace utils
 }  // namespace voxblox
 
 #endif  // GLOBAL_SEGMENT_MAP_LABEL_VOXEL_H_
