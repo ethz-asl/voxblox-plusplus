7a56dce8fe044dca2ea6dd60cee4b52a56ed26cf
ntonci <tonci.novkovic@gmail.com>
2019-09-21 06:42:15 +0200

    Sort features in a block and return only n best

--- global_segment_map/include/global_segment_map/label_tsdf_integrator.h
+++ global_segment_map/include/global_segment_map/label_tsdf_integrator.h
@@ -55,6 +55,7 @@ class LabelTsdfIntegrator : public MergedTsdfIntegrator {
     float lognormal_weight_sigma = 1.8f;
     float lognormal_weight_offset = 0.7f;
 
+    // TODO(ntonci): This doesn't really belong here.
     // ICP params.
     bool enable_icp = false;
     bool keep_track_of_icp_correction = false;
