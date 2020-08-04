4bd4c13e5051b4557ea6c0734d7804f48b4f9199
margaritaG <margaritagrinvald@gmail.com>
2020-08-04 16:24:59 +0200

    Stale bot and jenkins script

--- ci/prepare-jenkins-slave.sh
+++ ci/prepare-jenkins-slave.sh
@@ -1,3 +1,8 @@
 #!/bin/bash -e
-echo "Running the prepare script for voxblox_gsm.";
+echo "Running the prepare script for voxblox-plusplus.";
+
+# Install protobuf.
+sudo apt-get install protobuf-compiler libprotoc-dev
+
+# Set C++14.
 catkin config --cmake-args -DCMAKE_CXX_STANDARD=14
